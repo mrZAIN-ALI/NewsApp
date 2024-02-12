ce3cd2b5fb1dcac36d2478ae4c7c73a22e6db1d9
ZAIN ALI <97796695+mrZAIN-ALI@users.noreply.github.com>
2024-02-13 02:01:21 +0500

    first comiit

---
+++ dartFrog/routes/articles/breaking.dart
@@ -0,0 +1,5 @@
+import 'package:dart_frog/dart_frog.dart';
+
+Response onRequest(RequestContext context) {
+  return Response(body: 'This returns response body for breaking news articles');
+}
---
+++ dartFrog/routes/articles/popular.dart
@@ -0,0 +1,6 @@
+import 'package:dart_frog/dart_frog.dart';
+
+Response onRequest(RequestContext context) {
+  return Response(
+      body: 'This will return The response body for popular articles s');
+}
